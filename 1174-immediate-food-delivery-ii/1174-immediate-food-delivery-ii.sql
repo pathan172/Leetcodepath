39781ee663c62fa1b44b1570e1544c54db125008
MD ANZAR KHAN <144884339+pathan172@users.noreply.github.com>
2026-08-27 22:53:07 +0530

    Time: 725 ms (43.25%), Space: 0B (100%) - LeetHub

---
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -0,0 +1,25 @@
+# Write your MySQL query statement below
+-- Write your PostgreSQL query statement below
+WITH first_orders AS (
+    SELECT
+        customer_id,
+        MIN(order_date) AS first_order_date
+    FROM Delivery
+    GROUP BY customer_id
+)
+
+SELECT
+    ROUND(
+        100.0 * SUM(
+            CASE
+                WHEN d.order_date = d.customer_pref_delivery_date
+                THEN 1
+                ELSE 0
+            END
+        ) / COUNT(*),
+        2
+    ) AS immediate_percentage
+FROM Delivery d
+JOIN first_orders f
+    ON d.customer_id = f.customer_id
+    AND d.order_date = f.first_order_date;
